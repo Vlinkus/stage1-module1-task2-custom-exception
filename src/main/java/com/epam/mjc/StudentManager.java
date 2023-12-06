21206433b306897028b15b1e623823e223fbd2f6
VLinkus <LinkusVyte@gmail.com>
2023-12-06 11:53:08 +0200

    M1 Task2 Completed

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,7 +6,13 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    try{
+      Student student = Student.getValueOf(studentID);
+      if(student == null) throw new StudentNotFoundException(String.valueOf(studentID));
+      return student;
+    }catch (IllegalArgumentException exception){
+      throw new IllegalArgumentException("Could not find student with ID "+studentID);
+    }
   }
 
   public static void main(String[] args) {
